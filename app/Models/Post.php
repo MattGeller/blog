1dcab81018573ce406105a6e7cd8a62d24e6a481
Matt Geller <mattg92@gmail.com>
2022-09-24 00:48:05 -0700

    Initial commit

---
+++ app/Models/Post.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\File;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
+
+class Post
+{
+    public string $title;
+    public string $excerpt;
+    public $date;
+    public string $body;
+    public string $slug;
+
+    public function __construct(string $title, string $excerpt, $date, string $body, string $slug)
+    {
+        //TODO: refactor to use constructor property promotion
+        $this->title = $title;
+        $this->excerpt = $excerpt;
+        $this->date = $date;
+        $this->body = $body;
+        $this->slug = $slug;
+    }
+
+    public static function all(): Collection
+    {
+        return cache()->rememberForever('posts.all', function () {
+            return collect(File::files(resource_path('posts')))
+                ->map(fn($file) => YamlFrontMatter::parseFile($file))
+                ->map(fn($document) => new Post(
+                    $document->title,
+                    $document->excerpt,
+                    $document->date,
+                    $document->body(),
+                    $document->slug
+                ))->sortByDesc('date');
+        });
+    }
+
+    public static function find(string $slug)
+    {
+        return static::all()->firstWhere('slug', $slug);
+
+//        $path = resource_path("posts/$slug.html");
+//
+//        if (!file_exists($path)) {
+//            throw new ModelNotFoundException();
+//            // return redirect('/');
+//            // // abort(404);
+//        }
+//
+//        return cache()->remember("posts.$slug", 5, fn() => file_get_contents($path));
+    }
+}
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,9 @@
+<!doctype html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="/app.css">
+{{-- <script src="/app.js"></script> --}}
+
+<body>
+    @yield('content')
+</body>
