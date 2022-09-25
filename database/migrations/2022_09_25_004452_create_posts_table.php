d3ea3a0839e4b8dcbec1ef6e44964e50f708e95f
Matt Geller <mattg92@gmail.com>
2022-09-25 15:30:19 -0700

    Implement authors

--- database/migrations/2022_09_25_004452_create_posts_table.php
+++ database/migrations/2022_09_25_004452_create_posts_table.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Models\Category;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -16,7 +15,8 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->foreignIdFor(Category::class);
+            $table->foreignId('user_id');
+            $table->foreignId('category_id');
             $table->string('slug')->unique();
             $table->string('title');
             $table->text('excerpt');
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\Category;
 use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -16,7 +17,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+         $user = User::factory()->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
@@ -37,6 +38,7 @@ class DatabaseSeeder extends Seeder
         ]);
 
         Post::create([
+            'user_id' => $user->id,
             'category_id' => $personalCategory->id,
             'title' => 'My Personal Post',
             'slug' => 'my-personal-post',
@@ -44,6 +46,7 @@ class DatabaseSeeder extends Seeder
             'body' => '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>'
         ]);
         Post::create([
+            'user_id' => $user->id,
             'category_id' => $workCategory->id,
             'title' => 'My Work Post',
             'slug' => 'my-work-post',
@@ -51,6 +54,7 @@ class DatabaseSeeder extends Seeder
             'body' => '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>'
         ]);
         Post::create([
+            'user_id' => $user->id,
             'category_id' => $hobbyCategory->id,
             'title' => 'My Hobby Post',
             'slug' => 'my-hobby-post',
