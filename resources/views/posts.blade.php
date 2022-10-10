f3857c2db4c36fcb47caeba15fb0fa72388dce02
Matt Geller <mattg92@gmail.com>
2022-10-09 23:52:38 -0700

    Improve HTML and templates

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,34 +1,11 @@
 <x-layout>
-{{--    @foreach ($posts as $post)--}}
-{{--        <article>--}}
-{{--            <h1>--}}
-{{--                <a href="/posts/{{$post->slug}}">--}}
-{{--                    {{ $post->title }}--}}
-{{--                </a>--}}
-{{--            </h1>--}}
-
-{{--            <x-author-and-category :post="$post"></x-author-and-category>--}}
-
-{{--            <div>--}}
-{{--                {{ $post->excerpt }}--}}
-{{--            </div>--}}
-{{--        </article>--}}
-{{--    @endforeach--}}
-
     @include('_posts-header')
 
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
-        <x-post-featured-card/>
-
-        <div class="lg:grid lg:grid-cols-2">
-            <x-post-card/>
-            <x-post-card/>
-        </div>
-
-        <div class="lg:grid lg:grid-cols-3">
-            <x-post-card/>
-            <x-post-card/>
-            <x-post-card/>
-        </div>
+        @if ($posts->count())
+            <x-post-grid :posts="$posts"/>
+        @else
+            <p class="text-center">No post yet. Please check back later.</p>
+        @endif
     </main>
 </x-layout>
