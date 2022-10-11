9925916be02ea60128f971e12bbdeee6e029fdbc
Matt Geller <mattg92@gmail.com>
2022-10-11 15:34:36 -0700

    Improve dropdown again

--- resources/views/_posts-header.blade.php
+++ resources/views/_posts-header.blade.php
@@ -41,7 +41,9 @@
 
                     @foreach($categories as $category)
                         <a href="/categories/{{$category->slug}}"
-                           class="block text-left px-3 text-sm leading-6 hover:bg-blue-500 focus:bg-blue-500 hover:text-white focus:text-white">
+                           class="block text-left px-3 text-sm leading-6 hover:bg-blue-500 focus:bg-blue-500 hover:text-white focus:text-white
+                            {{isset($currentCategory) && $currentCategory->is($category) ? 'bg-blue-500 text-white' : ''}}"
+                        >
                             {{ ucwords($category->name) }}
                         </a>
                     @endforeach
